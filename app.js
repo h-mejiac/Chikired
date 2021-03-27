a8352f67a8eb6df73f99bd459e04fff78189a8b8
h-mejiac <hmejiac02@gmail.com>
2021-03-27 16:56:51 -0600

    declaración del servidor

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// cargar rutas
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//configurar cabeceras http
+
+// rutas base
+
+module.exports = app;
