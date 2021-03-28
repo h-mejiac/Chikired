5102867cf2e0c23a835cc454bf50f7a4a4b3e9d3
h-mejiac <hmejiac02@gmail.com>
2021-03-27 18:49:07 -0600

    Quitar aviso de base de datos

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var mongoClient = require("mongodb").MongoClient;
 var app = require('./app');
 var port = process.env.PORT || 3977;
 
+mongoClient.Promise = global.Promise;
 mongoClient.connect("mongodb://chikired-db:Lh4phrF0sbhfDoC8fWkf8KumLjENb9Q6J0b8dP5verRJ5NyLWE2jyk5M8E956o7gjzCjWQ49cnmKkEBfOb4MEg==@chikired-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@chikired-db@",
     function(err, db) {
         if (err) {
