ae029488e2bf195a2c35a299bbe65948c06c37d8
h-mejiac <hmejiac02@gmail.com>
2021-03-27 19:04:10 -0600

    Definicion de ruta

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // cargar rutas
+var usuario_routes = require('./routes/usuario');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,8 +14,6 @@ app.use(bodyParser.json());
 //configurar cabeceras http
 
 // rutas base
-app.get('/pruebas', function(req, res) {
-    res.status(200).send({ message: 'Bienvenido' });
-});
+app.use('/api', usuario_routes);
 
 module.exports = app;
