0d3c4c077f5897b6dd01672237c54e2733fcb227
h-mejiac <hmejiac02@gmail.com>
2021-03-28 15:02:35 -0600

    Asignación de cabeceras

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // configurar cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Controll-Allow-Origin', '*');
+    res.header('Access-Controll-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Request-With, Content-Type, Accept, Access-Control-Allow-Request-Methid');
+    res.header('Access-Controll-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+    next();
+});
 // rutas base
 app.use('/api', usuario_rutas);
 
